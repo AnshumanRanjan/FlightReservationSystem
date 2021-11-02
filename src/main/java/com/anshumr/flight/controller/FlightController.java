6c0489cf1c6214e725a2855ae3b469f68f3061fb
Anshuman <anshuman.ranjan01@gmail.com>
2021-11-02 20:49:50 +0000

    refactoring and linting the code

--- src/main/java/com/anshumr/flight/controller/FlightController.java
+++ src/main/java/com/anshumr/flight/controller/FlightController.java
@@ -25,206 +25,182 @@ import java.util.List;
 
 @RestController
 public class FlightController {
-	
+
 	@Autowired
-    private FlightDetailRepository flightDetailRepository;
+	private FlightDetailRepository flightDetailRepository;
 	UserSelection userSel;
 	FlightSeatReservation reser;
-	
 
 	@GetMapping(value = "/flight/search")
-    public  String createUserPref( @RequestParam SeatType seatType, @RequestParam SeatSection seatSelection,@RequestParam int NumberofTickets,@RequestParam Date tripdate,@RequestParam long numb) {
-    	
-    	
-    	userSel =  new UserSelection(seatType,seatSelection , NumberofTickets , tripdate ,numb );
-    	reser = new FlightSeatReservation(numb, tripdate, userSel,flightDetailRepository);
-    	return makeTableHTML(reser.returnSeatavailibility());
+	public String createUserPref(@RequestParam SeatType seatType, @RequestParam SeatSection seatSelection,
+			@RequestParam int NumberofTickets, @RequestParam Date tripdate, @RequestParam long numb) {
 
-    }
-    
-    
-    @RequestMapping(value = "/flight/{reserve}")
-    public  String submitUserPref( @RequestBody String SeatNo) {
-    	
-    	/* Case 1 user selected Auto Select seats */
-    	if (SeatNo.equalsIgnoreCase("SeatNo=")){
-    		System.out.println("Inside the autoselect method" + userSel.getSeatType() + "<--");
-    		
-    		/* Selecting Optimum Seat Selection  */
-    		// Calling getSeatpreference and getAreaPreference 
-    		
-    		getSeatPreference(getAreaPreference());
-    		
-        	return makeTableHTML(reser.returnSeatavailibility());
-    		
-    	}
-    	
-    	
-    	
-    	/* Case 2  , user provided a list of seats */
-    	// :TODO verify if seat number length = seat number requested  
-    	
-    	try {
-    		SeatNo = java.net.URLDecoder.decode(SeatNo, StandardCharsets.UTF_8.name());
-    	} catch (UnsupportedEncodingException e) {
-    	    // not going to happen - value came from JDK's own StandardCharsets
-    	}
-    	if (SeatNo == null) {
-    		return "Error , PLease select a Seat number ";
-    	}
-    	return makeTableHTML(reser.returnSeatavailibility(SeatNo.split("=")[1].split(",")));
-   
+		userSel = new UserSelection(seatType, seatSelection, NumberofTickets, tripdate, numb);
+		reser = new FlightSeatReservation(numb, tripdate, userSel, flightDetailRepository);
+		return makeTableHTML(reser.returnSeatavailibility());
 
-    }
-    
-    
-    @RequestMapping("/")
-    public String index() {
-        return "index";
-    }   
- 
-    
+	}
 
-    @RequestMapping(value = "/flight/reserveSeats/{flightNumber}/{searNo}/{modifiedFlag}")
-    public ResponseEntity<?> reserveSeats(@PathVariable int flightNumber, @PathVariable List<String> searNo,
-                                          @PathVariable int modifiedFlag) {
+	@RequestMapping(value = "/flight/{reserve}")
+	public String submitUserPref(@RequestBody String SeatNo) {
 
-    	
-    	return new ResponseEntity<>( "Basd on  modifiedFlag  either call autoAssign or createReservation", HttpStatus.OK);
+		/* Case 1 user selected Auto Select seats */
+		if (SeatNo.equalsIgnoreCase("SeatNo=")) {
+			System.out.println("Inside the autoselect method" + userSel.getSeatType() + "<--");
 
-       
-    }
-    
-    
-    
-   /* utility function for HTMl objects */
-    
- public String makeTableHTML(String[][] twoArray) { 
-	 String result = "<html><body><h1>Seat Selection<h1>" + "<table ,td , th style=\"border:2px solid black;width:60%;margin-left:auto;margin-right:auto;\" >";
-	    for(int i=0; i<twoArray.length; i++) {
-	        result += "<tr>";
-	        for(int j=0; j<twoArray[i].length; j++){
-	            result += "<td>"+twoArray[i][j]+"</td>";
-	        }
-	        result += "</tr>";
-	    }
-	    result += "</table>";
-	    
-	    /* Adding Submit preference and a AutoAssign Button, */
-	    String submissionForum = "<form action=\"/flight/reserve\" method = \"post\" enctype=\"application/json\" > <br> <br>  <label for=\"seatNo\">Seat number:</label>  <input type=\"text\" name=\"SeatNo\" value=\"\"/><input type=\"submit\" value=\"Submit Preference\">  <input type=\"submit\" value=\"Auto Assign Seat\"> </form>";
-	    result += submissionForum;
-	    result += "</body></html>";
-	    return result;
-	 
- }
- 
- 
- public Integer[] getAreaPreference() {
-	 
-	 int startfrom = 0 ;
-	 int endat = 0;
-	 Integer[] ret = new Integer[2];
-	 if (userSel.getSeatSelection() == SeatSection.FRONT) {
-		ret[0] = 1 ;
-		ret[1] = 3;
-		
-		 
-	 }
-	 
-    if (userSel.getSeatSelection() == SeatSection.MID) {
-    	ret[0] = 4 ;
-		ret[1] = 9;
-    	
-	 }
-    
-    if (userSel.getSeatSelection() == SeatSection.RARE) {
-    	ret[0] = 9 ;
-		ret[1] = 11;
- }
-    return ret;
- }
- 
- public void getSeatPreference(Integer[] areaPref) {
-	 
+			/* Selecting Optimum Seat Selection */
+			// Calling getSeatpreference and getAreaPreference
+
+			getSeatPreference(getAreaPreference());
+
+			return makeTableHTML(reser.returnSeatavailibility());
+
+		}
+
+		/* Case 2 , user provided a list of seats */
+		// :TODO verify if seat number length = seat number requested
+
+		try {
+			SeatNo = java.net.URLDecoder.decode(SeatNo, StandardCharsets.UTF_8.name());
+		} catch (UnsupportedEncodingException e) {
+			// not going to happen - value came from JDK's own StandardCharsets
+		}
+		if (SeatNo == null) {
+			return "Error , PLease select a Seat number ";
+		}
+		return makeTableHTML(reser.returnSeatavailibility(SeatNo.split("=")[1].split(",")));
+
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/flight/reserveSeats/{flightNumber}/{searNo}/{modifiedFlag}")
+	public ResponseEntity<?> reserveSeats(@PathVariable int flightNumber, @PathVariable List<String> searNo,
+			@PathVariable int modifiedFlag) {
+
+		return new ResponseEntity<>("Basd on  modifiedFlag  either call autoAssign or createReservation",
+				HttpStatus.OK);
+
+	}
+
+	/* utility function for HTMl objects */
+
+	public String makeTableHTML(String[][] twoArray) {
+		String result = "<html><body><h1>Seat Selection<h1>"
+				+ "<table ,td , th style=\"border:2px solid black;width:60%;margin-left:auto;margin-right:auto;\" >";
+		for (int i = 0; i < twoArray.length; i++) {
+			result += "<tr>";
+			for (int j = 0; j < twoArray[i].length; j++) {
+				result += "<td>" + twoArray[i][j] + "</td>";
+			}
+			result += "</tr>";
+		}
+		result += "</table>";
+
+		/* Adding Submit preference and a AutoAssign Button, */
+		String submissionForum = "<form action=\"/flight/reserve\" method = \"post\" enctype=\"application/json\" > <br> <br>  <label for=\"seatNo\">Seat number:</label>  <input type=\"text\" name=\"SeatNo\" value=\"\"/><input type=\"submit\" value=\"Submit Preference\">  <input type=\"submit\" value=\"Auto Assign Seat\"> </form>";
+		result += submissionForum;
+		result += "</body></html>";
+		return result;
+
+	}
+
+	public Integer[] getAreaPreference() {
+
+		int startfrom = 0;
+		int endat = 0;
+		Integer[] ret = new Integer[2];
+		if (userSel.getSeatSelection() == SeatSection.FRONT) {
+			ret[0] = 1;
+			ret[1] = 3;
+
+		}
+
+		if (userSel.getSeatSelection() == SeatSection.MID) {
+			ret[0] = 4;
+			ret[1] = 9;
+
+		}
+
+		if (userSel.getSeatSelection() == SeatSection.RARE) {
+			ret[0] = 9;
+			ret[1] = 11;
+		}
+		return ret;
+	}
+
+	public void getSeatPreference(Integer[] areaPref) {
+
 		int startro = areaPref[0];
-	    int endRow = areaPref[1];
-	 
-	 if (userSel.getSeatType() == SeatType.WINDOW) {
+		int endRow = areaPref[1];
+
+		if (userSel.getSeatType() == SeatType.WINDOW) {
 			// Loop across the row to see which window seats are vacent
-			
-			for (int i = 0 ; i < userSel.getNumOfTickets() ;i++)
-			{
+
+			for (int i = 0; i < userSel.getNumOfTickets(); i++) {
 				int j = startro;
 				while (j < endRow) {
-				if (reser.returnSeatavailibility()[j][1] != "X") {
-					reser.returnSeatavailibility()[j][1] = "X";
-					break;
-				}
-				else if (reser.returnSeatavailibility()[j][8] != "X")
-				{
-					reser.returnSeatavailibility()[j][8] = "X";
-					break;
-				}
-				else { j++;}
+					if (reser.returnSeatavailibility()[j][1] != "X") {
+						reser.returnSeatavailibility()[j][1] = "X";
+						break;
+					} else if (reser.returnSeatavailibility()[j][8] != "X") {
+						reser.returnSeatavailibility()[j][8] = "X";
+						break;
+					} else {
+						j++;
+					}
 				}
 			}
-			
+
 		}
 		if (userSel.getSeatType() == SeatType.AISL) {
 			// Loop across the row to see which window seats are vacent
-			
-			for (int i = 0 ; i < userSel.getNumOfTickets() ;i++)
-			{
+
+			for (int i = 0; i < userSel.getNumOfTickets(); i++) {
 				int j = startro;
 				while (j < endRow) {
-				if (reser.returnSeatavailibility()[j][2] != "X") {
-					reser.returnSeatavailibility()[j][2] = "X";
-					break;
-				}
-				else if (reser.returnSeatavailibility()[j][3] != "X")
-				{
-					reser.returnSeatavailibility()[j][3] = "X";
-					break;
-				}
-				else if (reser.returnSeatavailibility()[j][6] != "X")
-				{
-					reser.returnSeatavailibility()[j][6] = "X";
-					break;
-				}
-				else if (reser.returnSeatavailibility()[j][7] != "X")
-				{
-					reser.returnSeatavailibility()[j][7] = "X";
-					break;
-				}
-				else { j++;}
+					if (reser.returnSeatavailibility()[j][2] != "X") {
+						reser.returnSeatavailibility()[j][2] = "X";
+						break;
+					} else if (reser.returnSeatavailibility()[j][3] != "X") {
+						reser.returnSeatavailibility()[j][3] = "X";
+						break;
+					} else if (reser.returnSeatavailibility()[j][6] != "X") {
+						reser.returnSeatavailibility()[j][6] = "X";
+						break;
+					} else if (reser.returnSeatavailibility()[j][7] != "X") {
+						reser.returnSeatavailibility()[j][7] = "X";
+						break;
+					} else {
+						j++;
+					}
 				}
 			}
-			
+
 		}
 		if (userSel.getSeatType() == SeatType.MIDDLE) {
 			// Loop across the row to see which window seats are vacent
-			
-			for (int i = 0 ; i < userSel.getNumOfTickets() ;i++)
-			{
+
+			for (int i = 0; i < userSel.getNumOfTickets(); i++) {
 				int j = startro;
 				while (j < endRow) {
-				if (reser.returnSeatavailibility()[j][4] != "X") {
-					reser.returnSeatavailibility()[j][4] = "X";
-					break;
-				}
-				else if (reser.returnSeatavailibility()[j][4] != "X")
-				{
-					reser.returnSeatavailibility()[j][4] = "X";
-					break;
-				}
-				else { j++;}
+					if (reser.returnSeatavailibility()[j][4] != "X") {
+						reser.returnSeatavailibility()[j][4] = "X";
+						break;
+					} else if (reser.returnSeatavailibility()[j][4] != "X") {
+						reser.returnSeatavailibility()[j][4] = "X";
+						break;
+					} else {
+						j++;
+					}
 				}
 			}
-			
+
 		}
-	 
- }
- 
-    
-    
+
+	}
+
 }
--- src/main/java/com/anshumr/flight/model/FlightSeatReservation.java
+++ src/main/java/com/anshumr/flight/model/FlightSeatReservation.java
@@ -15,55 +15,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.anshumr.flight.repository.FlightDetailRepository;
 
-
 public class FlightSeatReservation {
-	
-	
-	
-	
-    private long flightno;
+
+	private long flightno;
 	private Date flightDate;
-	private String[][] seats ;
-	
+	private String[][] seats;
+
 	private int totalSeats;
 	static int filled = 0;
 
-	public FlightSeatReservation(long flightNumber, Date flightDate , UserSelection userSel , FlightDetailRepository flightDetailRepository) {
-		
-		
+	public FlightSeatReservation(long flightNumber, Date flightDate, UserSelection userSel,
+			FlightDetailRepository flightDetailRepository) {
+
 		FlightDetail flight = flightDetailRepository.findByFlightno(flightNumber);
-		
+
 		System.out.println("Query Returned " + flight.getNumRows());
 		this.flightDate = flightDate;
 
-		this.seats = new String[flight.getNumRows()+1][flight.getNumColumns()+1];
-		this.totalSeats = flight.getNumRows()*flight.getNumColumns();	
+		this.seats = new String[flight.getNumRows() + 1][flight.getNumColumns() + 1];
+		this.totalSeats = flight.getNumRows() * flight.getNumColumns();
 
-		
-		//  Filling the row lables with Row Number 
-			seats[0][0] = "0";
-			seats[0][1] = "A";
-			seats[0][2] = "B";
-			seats[0][3] = "C";
-			seats[0][4] = "D";
-			seats[0][5] = "E";
-			seats[0][6] = "F";
-			seats[0][7] = "G";
-			seats[0][8] = "H";
-		
-			seats[0][0] = "0";
-			seats[1][0] = "1";
-			seats[2][0] = "2";
-			seats[3][0] = "3";
-			seats[4][0] = "4";
-			seats[5][0] = "5";
-			seats[6][0] = "6";
-			seats[7][0] = "7";
-			seats[8][0] = "8";
-			seats[9][0] = "9";
-			seats[10][0] = "10";
-		
-		// Filling the column labels 
+		// Filling the row lables with Row Number
+		seats[0][0] = "0";
+		seats[0][1] = "A";
+		seats[0][2] = "B";
+		seats[0][3] = "C";
+		seats[0][4] = "D";
+		seats[0][5] = "E";
+		seats[0][6] = "F";
+		seats[0][7] = "G";
+		seats[0][8] = "H";
+
+		seats[0][0] = "0";
+		seats[1][0] = "1";
+		seats[2][0] = "2";
+		seats[3][0] = "3";
+		seats[4][0] = "4";
+		seats[5][0] = "5";
+		seats[6][0] = "6";
+		seats[7][0] = "7";
+		seats[8][0] = "8";
+		seats[9][0] = "9";
+		seats[10][0] = "10";
+
+		// Filling the column labels
 		for (int i = 1; i <= flight.getNumRows(); i++) {
 			seats[i][1] = "O";
 			seats[i][2] = "O";
@@ -75,112 +70,89 @@ public class FlightSeatReservation {
 			seats[i][8] = "O";
 		}
 
-
-
-
 	}
 
-	public  String[][] returnSeatavailibility() {
-		
-		//System.out.println(Arrays.deepToString(seats).replace("], ", "]\n"));
-		//return Arrays.deepToString(seats).replace("], ", "]\n") ;
-		
+	public String[][] returnSeatavailibility() {
+
+		// System.out.println(Arrays.deepToString(seats).replace("], ", "]\n"));
+		// return Arrays.deepToString(seats).replace("], ", "]\n") ;
+
 		return seats;
 
-
 	}
-	
-	
-	/*  method overrides to generate reserved Seats */ 
-	
-    public  String[][] returnSeatavailibility(String[] seatNo) {
-		
-    	
-    	String rowNum  ;
-    	String colNum  ;
 
-		for (int i =0; i<seatNo.length;i++) {
-			if (seatNo[i].length() == 2)
-			{
-			rowNum =  seatNo[i].substring(1, 2);
-			colNum = seatNo[i].substring(0,1);
+	/* method overrides to generate reserved Seats */
 
+	public String[][] returnSeatavailibility(String[] seatNo) {
+
+		String rowNum;
+		String colNum;
+
+		for (int i = 0; i < seatNo.length; i++) {
+			if (seatNo[i].length() == 2) {
+				rowNum = seatNo[i].substring(1, 2);
+				colNum = seatNo[i].substring(0, 1);
+
+			} else if (seatNo[i].length() == 3) {
+				rowNum = seatNo[i].substring(1, 2);
+				colNum = seatNo[i].substring(0, 0);
+			} else {
+				rowNum = "Error";
+				colNum = "Error";
 			}
-			else if (seatNo[i].length() == 3)
-			{
-			rowNum = seatNo[i].substring(1,2) ;
-			colNum = seatNo[i].substring(0,0);
-			}
-			else {
-				rowNum ="Error";
-				colNum="Error";
-			}
-			this.researveSeats(Integer.parseInt(rowNum),colNum);
-			rowNum="";
-			colNum="";	
+			this.researveSeats(Integer.parseInt(rowNum), colNum);
+			rowNum = "";
+			colNum = "";
 		}
-			
-		
+
 		return seats;
 
+	}
+
+	public void researveSeats(int row, String col) {
+
+		// Check if input is valid
+		int column = (int) col.charAt(0) - 64;
+		seats[row][column] = "X";
 
 	}
-	
-    
-    public  void researveSeats(int row, String col) {
-    	
-    // Check if input is valid 
-    	int column = (int)col.charAt(0) - 64 ;
-    	seats[row][column] = "X";
-    	
-    	
-    }
-	
 
-	public Boolean researveSeats(List<String> seatNumber ,FlightDetailRepository flightDetailRepository  ) {
+	public Boolean researveSeats(List<String> seatNumber, FlightDetailRepository flightDetailRepository) {
 
-		// CHeck if the number of available seats are not less than number of seats asked in the list :TODO
+		// CHeck if the number of available seats are not less than number of seats
+		// asked in the list :TODO
 
 		FlightDetail flight = flightDetailRepository.findByFlightno(flightno);
-		for (String seat : seatNumber ) {
+		for (String seat : seatNumber) {
 
-				int rownum = seat.charAt(0) - '1';
-				int colnum = seat.charAt(1) - 'A';
+			int rownum = seat.charAt(0) - '1';
+			int colnum = seat.charAt(1) - 'A';
 
-				// Verify that the seat input are valid and correct :TODO
-				
-				if (rownum < 0 || rownum > flight.getNumRows() || colnum < 0 || colnum > flight.getNumColumns()) {
-					System.out.println("Seat selected are not valid");
-					}
+			// Verify that the seat input are valid and correct :TODO
 
-				else {
+			if (rownum < 0 || rownum > flight.getNumRows() || colnum < 0 || colnum > flight.getNumColumns()) {
+				System.out.println("Seat selected are not valid");
+			}
 
-					if (seats[rownum][colnum] != "X") {
-						seats[rownum][colnum] = "X";
-						filled++;
-						
-						return true;
-					}
-					else
-					{
-						System.out.println("Selectd Seats are already researved ");
-					}
+			else {
 
+				if (seats[rownum][colnum] != "X") {
+					seats[rownum][colnum] = "X";
+					filled++;
+
+					return true;
+				} else {
+					System.out.println("Selectd Seats are already researved ");
 				}
+
+			}
 
 		}
 		return false;
 	}
 
-	
-	
-	public void autoResearveSeats(List<String> seatNumber , UserSelection pref )
-	{
-		
-		
-		
+	public void autoResearveSeats(List<String> seatNumber, UserSelection pref) {
+
 	}
-
-
 
 }
